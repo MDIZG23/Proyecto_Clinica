766a7c36fe47514e4c84dd67d3d45513e33f1cc8
MDIZG23 <152795882+MDIZG23@users.noreply.github.com>
2023-12-03 09:48:13 -0600

    CODIGO DE PRODUCTO

--- resources/views/Producto.Balde.php
+++ resources/views/Producto.Balde.php
@@ -1,102 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Personas</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
 
-<div class="container">
-  <h2>Personas</h2>
-  <p>Lista de Personas</p>
+    <div class="container">
+        <h2>Productos</h2>
+        <p>Lista de Productos</p>
 
-  <table class="table table-bordered table-sm">
-    <thead>
-      <tr>
-        <td>IDENTIDAD</td>
-        <td>PRIMERNOMBRE</td>
-        <td>SEGUNDONOMBRE</td>
-        <td>PRIMERAPELLIDO</td>
-        <td>SEGUNDOAPELLIDO</td>
-        <td>SEXPERSONA</td>
-        <td>INDCIVIL</td>
-        <td>EDADPERSONA</td>
-        <td>TELEFONO</td>
-        <td>CORREO</td>
-        <td>DIRPERSONA</td>
-        <td>FECREGISTRO</td>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($ResulPersonas as $persona)
-        <tr>
-          <td class="inner-table">{{$persona["id"]}}</td>
-          <td class="inner-table">{{$persona["firstname"]}}</td>
-          <td class="inner-table">{{$persona["secondname"]}}</td>
-          <td class="inner-table">{{$persona["surname"]}}</td>
-          <td class="inner-table">{{$persona["secondsurname"]}}</td>
-          <td class="inner-table">{{$persona["sexperson"]}}</td>
-          <td class="inner-table">{{$persona["civilstatus"]}}</td>
-          <td class="inner-table">{{$persona["age"]}}</td>
-          <td class="inner-table">{{$persona["phone"]}}</td>
-          <td class="inner-table">{{$persona["mail"]}}</td>
-          <td class="inner-table">{{$persona["address"]}}</td>
-          <td class="inner-table">{{$persona["registrationdate"]}}</td>
-        </tr>
-      @endforeach
-    </tbody>
-  </table>
+        <table class="table table-bordered table-sm">
+            <thead>
+                <tr>
+                    <td>ID Producto</td>
+                    <td>Nombre</td>
+                    <td>Descripción</td>
+                    <td>Precio</td>
+                    <td>Categoría</td>
+                    <td>Estado</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($ResulProductos as $producto)
+                <tr>
+                    <td class="inner-table">{{$producto["id"]}}</td>
+                    <td class="inner-table">{{$producto["NOM_PRODUCTO"]}}</td>
+                    <td class="inner-table">{{$producto["DES_PRODUCTO"]}}</td>
+                    <td class="inner-table">{{$producto["PRECIO"]}}</td>
+                    <td class="inner-table">{{$producto["ID_CATEGORIA"]}}</td>
+                    <td class="inner-table">{{$producto["ID_ESTADO"]}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-  <h2>Insertar Nueva Persona</h2>
-  <form action="/INSERT_PERSONA" method="post">
-    @csrf <!-- Agrega esto para protección contra CSRF -->
-  
-    <label for="identidad">Identidad:</label>
-    <input type="text" name="PV_IDENTIDAD" required>
+        <h2>Insertar Nuevo Producto</h2>
+        <form action="/INSERT_PRODUCTO" method="post">
+            @csrf <!-- Agrega esto para protección contra CSRF -->
 
-    <label for="primerNombre">Primer Nombre:</label>
-    <input type="text" name="PV_PRIMER_NOMBRE" required>
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="PV_NOM_PRODUCTO" required>
 
-    <label for="segundoNombre">Segundo Nombre:</label>
-    <input type="text" name="PV_SEGUNDO_NOMBRE" required>
+            <label for="descripcion">Descripción:</label>
+            <input type="text" name="PV_DES_PRODUCTO" required>
 
-    <label for="primerApellido">Primer Apellido:</label>
-    <input type="text" name="PV_PRIMER_APELLIDO" required>
+            <label for="precio">Precio:</label>
+            <input type="number" step="0.01" name="PN_PRECIO" required>
 
-    <label for="segundoApellido">Segundo Apellido:</label>
-    <input type="text" name="PV_SEGUNDO_APELLIDO" required>
+            <label for="categoria">Categoría:</label>
+            <input type="text" name="PN_ID_CATEGORIA" required>
 
-    <label for="sexo">Sexo:</label>
-    <select name="PV_SEX_PERSONA" required>
-      <option value="M">Masculino</option>
-      <option value="F">Femenino</option>
-    </select>
+            <label for="estado">Estado:</label>
+            <input type="text" name="PN_ID_ESTADO" required>
 
-    <label for="estadoCivil">Estado Civil:</label>
-    <input type="text" name="PV_IND_CIVIL" required>
+            <button type="submit">Insertar</button>
+        </form>
+    </div>
 
-    <label for="edad">Edad:</label>
-    <input type="number" name="PN_EDAD_PERSONA" required>
-
-    <label for="telefono">Teléfono:</label>
-    <input type="text" name="PN_TELEFONO" required>
-
-    <label for="correo">Correo:</label>
-    <input type="email" name="PV_CORREO" required>
-
-    <label for="direccion">Dirección:</label>
-    <input type="text" name="PV_DIR_PERSONA" required>
-
-    <label for="fechaRegistro">Fecha de Registro:</label>
-    <input type="text" name="PV_FEC_REGISTRO" required>
-
-    <button type="submit">Insertar</button>
-  </form>
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
